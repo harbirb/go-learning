77b02a9ed884a4e22401f9f690ca37e3df27822d
bharbir <harbirbajwa@hotmail.com>
2024-09-09 16:16:38 -0700

    learned pointers and errors

---
+++ wallet/wallet.go
@@ -0,0 +1,43 @@
+package wallet
+
+import (
+	"errors"
+	"fmt"
+)
+
+
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+// args are copied, so need to pass a Wallet pointer instead of just wallet
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
